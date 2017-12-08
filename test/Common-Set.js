7e13cbb4d365f72b8dd070bf4acd77851b12eaab
haad <haad@headbanggames.com>
2017-12-08 19:53:03 +0100

    Add common tests for Sets

---
+++ test/Common-Set.js
@@ -0,0 +1,121 @@
+'use strict'
+
+const assert = require('assert')
+
+const GCounter = require('../src/G-Counter.js')
+const GSet = require('../src/G-Set.js')
+const TwoPSet = require('../src/2P-Set.js')
+const ORSet = require('../src/OR-Set.js')
+
+const added = [1, 2, 3]
+const removed = [1]
+const diff = ['one', 77]
+
+const crdts = [
+  {
+    type: 'G-Set',
+    class: GSet,
+    added: added,
+    removed: [],
+    diff: diff,
+    inputData: {
+      values: added,
+    },
+    expectedValues: added,
+    expectedValuesWithDiff: added.concat(diff),
+    create: (input) => new GSet(input && input.values ? input.values : []),
+    from: (json) => GSet.from(json),
+    remove: (crdt, tag) => 0,
+    isEqual: (a, b) => GSet.isEqual(a, b),
+    difference: (a, b) => GSet.difference(a, b),
+  },
+  {
+    type: '2P-Set',
+    class: TwoPSet,
+    added: added,
+    removed: removed,
+    diff: diff,
+    inputData: {
+      added: added,
+      removed: removed,
+    },
+    expectedValues: [2, 3],
+    expectedValuesWithDiff: added.concat(diff),
+    create: (input) => new TwoPSet(input && input.added ? input.added : [], input && input.removed ? input.removed : []),
+    from: (json) => TwoPSet.from(json),
+    remove: (crdt, tag) => crdt.remove(tag),
+    isEqual: (a, b) => TwoPSet.isEqual(a, b),
+    difference: (a, b) => TwoPSet.difference(a, b),
+  },
+  {
+    type: 'OR-Set',
+    class: ORSet,
+    added: added,
+    removed: removed,
+    diff: diff,
+    inputData: {
+      values: [
+        {
+          value: 'A',
+          _added: [1],
+          _removed: [],
+        },
+        {
+          value: 'B',
+          _added: [1],
+          _removed: [1],
+        },
+        {
+          value: 'C',
+          _added: [1, 2],
+          _removed: [2, 3],
+        },
+      ],
+    },
+    expectedValues: ['A', 'C'],
+    expectedValuesWithDiff: added.concat(diff),
+    create: (input) => new ORSet(input && input.values ? input.values : []),
+    from: (json) => ORSet.from(json),
+    remove: (crdt, tag) => crdt.remove(tag),
+    isEqual: (a, b) => ORSet.isEqual(a, b),
+    difference: (a, b) => ORSet.difference(a, b),
+  },
+]
+
+describe('Sets - Common', () => {
+  crdts.forEach(async (CRDT) => {
+    describe(CRDT.type, () => {
+      it('creates a new ' + CRDT.type + ' from a JSON object', () => {
+        const orset1 = CRDT.create(CRDT.inputData)
+        const orset2 = CRDT.from(CRDT.inputData)
+        assert.deepEqual(new Set(orset2.values), new Set(CRDT.expectedValues))
+      })
+
+      it('returns true if two Sets are equal', () => {
+        const orset1 = CRDT.create(CRDT.inputData)
+        const orset2 = CRDT.create(CRDT.inputData)
+        const orset3 = CRDT.create([])
+        assert.equal(CRDT.isEqual(orset1, orset2), true)
+        const isEqual = CRDT.isEqual(orset1, orset3)
+        assert.equal(isEqual, false)
+      })
+
+      it('returns a Set of values from Set A that are not in Set B', () => {
+        const addedValues = added
+        const removedValues = removed
+        const expectedDiff = CRDT.diff
+        const expectedValues = CRDT.expectedValuesWithDiff
+
+        const orset1 = CRDT.create()
+        const orset2 = CRDT.create()
+        const orset3 = CRDT.create()
+
+        addedValues.concat(expectedDiff).forEach(e => orset1.add(e, 0))
+        removedValues.forEach(e => CRDT.remove(orset1, 0))
+        addedValues.forEach(e => orset2.add(e, 0))
+        assert.deepEqual(CRDT.difference(orset1, orset2), new Set(expectedDiff))
+        assert.deepEqual(CRDT.difference(orset1, orset3), new Set(expectedValues))
+      })
+    })
+  })
+})
