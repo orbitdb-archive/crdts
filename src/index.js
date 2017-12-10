458d76d5856d7a21bc4f42a83452cb62cd14fa7a
haad <haad@headbanggames.com>
2017-12-10 11:28:48 +0100

    Add CmRDTSet type to module exports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // Export all supported CRDTs
-exports.GCounter = GCounter = require('./G-Counter.js')
-exports.GSet = GSet = require('./G-Set.js')
-exports.TwoPSet = TwoPSet = require('./2P-Set.js')
-exports.ORSet = ORSet = require('./OR-Set.js')
-exports.LWWSet = LWWSet = require('./LWW-Set.js')
-exports.PNCounter = PNCounter = require('./PN-Counter.js')
+exports.GCounter = GCounter = require('./G-Counter')
+exports.CmRDTSet = CmRDTSet = require('./CmRDT-Set')
+exports.GSet = GSet = require('./G-Set')
+exports.TwoPSet = TwoPSet = require('./2P-Set')
+exports.ORSet = ORSet = require('./OR-Set')
+exports.LWWSet = LWWSet = require('./LWW-Set')
 
 /**
  * CRDTs
@@ -20,9 +20,9 @@ exports.PNCounter = PNCounter = require('./PN-Counter.js')
  */
 module.exports = {
   GCounter: GCounter,
+  CmRDTSet: CmRDTSet,
   GSet: GSet,
   TwoPSet: TwoPSet,
   ORSet: ORSet,
   LWWSet: LWWSet,
-  PNCounter: PNCounter
 }
