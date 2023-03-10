5f92ecd97c744c3beadfbf89e872509475bca775
saul <saul@organicdesign.co.nz>
2023-03-10 13:24:07 +1300

    Convert CmRDT-Set to typescript.

--- src/CmRDT-Set.ts
+++ src/CmRDT-Set.ts
@@ -4,17 +4,17 @@ import { OperationTuple3 } from './utils.js'
  * CmRDT-Set
  *
  * Base Class for Operation-Based Set CRDT. Provides a Set interface.
- * 
+ *
  * Operations are described as:
- * 
+ *
  *   Operation = Tuple3(value : Any, added : Set, removed : Set)
- * 
- * This class is meant to be used as a base class for 
- * Operation-Based CRDTs that can be derived from Set 
+ *
+ * This class is meant to be used as a base class for
+ * Operation-Based CRDTs that can be derived from Set
  * semantics and which calculate the state (values)
  * based on a set of operations.
  *
- * Used by: 
+ * Used by:
  *   G-Set - https://github.com/orbitdb/crdts/blob/master/src/G-Set.js
  *   OR-Set - https://github.com/orbitdb/crdts/blob/master/src/OR-Set.js
  *   2P-Set - https://github.com/orbitdb/crdts/blob/master/src/2P-Set.js
@@ -24,30 +24,37 @@ import { OperationTuple3 } from './utils.js'
  * "A comprehensive study of Convergent and Commutative Replicated Data Types"
  * http://hal.upmc.fr/inria-00555588/document
  */
-export default class CmRDTSet extends Set {
+export default class CmRDTSet<V=unknown, T=unknown> extends Set<V> {
+	// Internal cache for tracking which values have been added to the set
+	protected _values = new Set<V>()
+
+	// List of operations (adds or removes of a value) for this set as
+	// Operation : Tuple3(value : Any, added : Set, removed : Set)
+	// added and removed can be any value, eg. it can be used to store
+	// timestamps/clocks for each operation in order to determine if
+	// a value is in the set
+	protected _operations: OperationTuple3<V, T>[]
+
+	protected _options: Partial<{ compareFunc: (a: V, b: V) => boolean }> = {}
+
   /**
    * Create a new CmRDTSet instance
    * @override
    *
-   * The constructor should never be used directly 
+   * The constructor should never be used directly
    * but rather via `super()` call in the constructor of
    * the class that inherits from CmRDTSet
-   * 
+   *
    * @param  {[Iterable]} iterable [Opetional Iterable object (eg. Array, Set) to create the Set from]
    * @param  {[Object]}   options  [Options to pass to the Set. Currently supported: `{ compareFunc: (a, b) => true|false }`]
    */
-  constructor (iterable, options) {
+  constructor (iterable?: Iterable<{ value: V, added: T[], removed: T[] }>, options?: Partial<{ compareFunc: (a: V, b: V) => boolean }>) {
     super()
-    // Internal cache for tracking which values have been added to the set
-    this._values = new Set()
-    // List of operations (adds or removes of a value) for this set as
-    // Operation : Tuple3(value : Any, added : Set, removed : Set)
-    // added and removed can be any value, eg. it can be used to store 
-    // timestamps/clocks for each operation in order to determine if 
-    // a value is in the set
-    this._operations = iterable ? iterable.map(OperationTuple3.from) : []
+
+    this._operations = iterable ? [...iterable].map(OperationTuple3.from) : []
+
     // Internal options
-    this._options = options || {}
+    this._options = options ?? {}
   }
 
   /**
@@ -55,9 +62,9 @@ export default class CmRDTSet extends Set {
    * @override
    * @return {[Set]} [Values in this set]
    */
-  values () {
-    const shouldIncludeValue = e => this._resolveValueState(e.added, e.removed, this._options.compareFunc)
-    const getValue = e => e.value
+  values (): IterableIterator<V> {
+    const shouldIncludeValue = (e: OperationTuple3<V, T>) => this._resolveValueState(e.added, e.removed, this._options.compareFunc)
+    const getValue = (e: OperationTuple3<V, T>) => e.value
     // Filter out values that should not be in this set
     // by using the _resolveValueState() function to determine
     // if the value should be present
@@ -72,7 +79,7 @@ export default class CmRDTSet extends Set {
    * @param  {[Any]}  value [Value to look for]
    * @return {Boolean}      [True if value is in the Set, false if not]
    */
-  has (value) {
+  has (value: V): boolean {
     return new Set(this.values()).has(value)
   }
 
@@ -81,9 +88,9 @@ export default class CmRDTSet extends Set {
    * @param  {[Array]}  values [Values that should be in the Set]
    * @return {Boolean}         [True if all values are in the Set, false if not]
    */
-  hasAll (values) {
-    const contains = e => this.has(e)
-    return values.every(contains) 
+  hasAll (values: V[]): boolean {
+    const contains = (e: V) => this.has(e)
+    return values.every(contains)
   }
 
   /**
@@ -94,20 +101,40 @@ export default class CmRDTSet extends Set {
    * for example the tag can be a clock or other identifier
    * that can be used to determine together with remove operations,
    * whether a value is included in the Set
-   * 
+   *
    * @param {[Any]} value [Value to add to the Set]
    * @param {[Any]} tag   [Optional tag for this add operation, eg. a clock]
    */
-  add (value, tag) {
+  /*
+  add (value: V, tag?: T): this {
     // If the value is not in the set yet
     if (!this._values.has(value)) {
       // Create an operation for the value and apply it to this set
-      const addOperation = OperationTuple3.create(value, [tag], null)
+      const addOperation = OperationTuple3.create<V, T>(value, tag != null ? [tag] : [], [])
+      this._applyOperation(addOperation)
+    } else {
+      // If the value is in the set, add a tag to its added set
+      if (tag != null)
+        this._findOperationsFor(value).map(val => val.added.add(tag))
+    }
+
+    return this
+  }
+	*/
+
+	// @ts-ignore TS2416 We are modifying the signature of Set here.
+  add (value: V, tag: T): this {
+    // If the value is not in the set yet
+    if (!this._values.has(value)) {
+      // Create an operation for the value and apply it to this set
+      const addOperation = OperationTuple3.create<V, T>(value, [tag])
       this._applyOperation(addOperation)
     } else {
       // If the value is in the set, add a tag to its added set
       this._findOperationsFor(value).map(val => val.added.add(tag))
     }
+
+    return this
   }
 
   /**
@@ -122,18 +149,18 @@ export default class CmRDTSet extends Set {
    * @param  {[Any]} value [Value to remove from the Set]
    * @param  {[Any]} tag   [Optional tag for this remove operation, eg. a clock]
    */
-  remove (value, tag) {
-    // Add a remove tag to the value's removed set, and only
-    // apply the remove operation if the value was added previously
-    this._findOperationsFor(value).map(e => e.removed.add(tag))
-  }
+	remove (value: V, tag: T): void {
+		// Add a remove tag to the value's removed set, and only
+		// apply the remove operation if the value was added previously
+		this._findOperationsFor(value).map(e => e.removed.add(tag))
+	}
 
   /**
    * Merge the Set with another Set
    * @override
    * @param  {[CRDTSet]} other [Set to merge with]
    */
-  merge (other) {
+  merge (other: CmRDTSet<V, T>): void {
     other._operations.forEach(operation => {
       const value = operation.value
       if (!this._values.has(value)) {
@@ -155,13 +182,13 @@ export default class CmRDTSet extends Set {
    * CmRDT-Set as an Object that can be JSON.stringified
    * @return {[Object]} [Object in the shape of `{ values: [ { value: <value>, added: [<tags>], removed: [<tags>] } ] }`]
    */
-  toJSON () {
+  toJSON (): { values: { value: V, added: T[], removed: T[] }[] } {
     const values = this._operations.map(e => {
       return {
         value: e.value,
         added: Array.from(e.added),
         removed: Array.from(e.removed),
-      }      
+      }
     })
     return { values: values }
   }
@@ -170,7 +197,7 @@ export default class CmRDTSet extends Set {
    * Create an Array of the values of this Set
    * @return {[Array]} [Values of this Set as an Array]
    */
-  toArray () {
+  toArray (): V[] {
     return Array.from(this.values())
   }
 
@@ -179,29 +206,29 @@ export default class CmRDTSet extends Set {
    * @param  {[type]}  other [Set to compare]
    * @return {Boolean}       [True if this Set is the same as the other Set]
    */
-  isEqual (other) {
+  isEqual (other: CmRDTSet<V, T>): boolean {
     return CmRDTSet.isEqual(this, other)
   }
 
   /**
    * _resolveValueState function is used to determine if an element is present in a Set.
-   * 
+   *
    * It receives a Set of add tags and a Set of remove tags for an element as arguments.
    * It returns true if an element should be included in the state and false if not.
-   * 
+   *
    * Overwriting this function gives us the ability to compare add/remove operations
    * of a particular element (value) in the set and determine if the value should be
    * included in the set or not. The function gets called once per element and returning
    * true will include the value in the set and returning false will exclude it from the set.
-   * 
+   *
    * @param  {[type]} added       [Set of added elements]
    * @param  {[type]} removed     [Set of removed elements]
    * @param  {[type]} compareFunc [Comparison function to compare elements with]
    * @return {[type]}             [true if element should be included in the current state]
    */
-  _resolveValueState (added, removed, compareFunc) {
+  protected _resolveValueState (added: Set<T>, removed: Set<T>, compareFunc?: (a: V, b: V) => boolean) {
     // By default, if there's an add operation present,
-    // and there are no remove operations, we include 
+    // and there are no remove operations, we include
     // the value in the set
     return added.size > 0 && removed.size === 0
   }
@@ -210,9 +237,9 @@ export default class CmRDTSet extends Set {
    * Add a value to the internal cache
    * @param {[OperationTuple3]} operationTuple3 [Tuple3(value, addedTags, removedTags)]
    */
-  _applyOperation (operationTuple3) {
+  protected _applyOperation (operationTuple3: OperationTuple3<V, T>) {
     this._operations.push(operationTuple3)
-    this._values.add(operationTuple3.value)  
+    this._values.add(operationTuple3.value)
   }
 
   /**
@@ -224,16 +251,17 @@ export default class CmRDTSet extends Set {
    *
    * Where 'value' is the value in the set, 'added' is all add operations
    * and 'removed' are all remove operations for that value.
-   * 
+   *
    * @param  {[Any]} value [Value to find]
    * @return {[Any]}       [Value if found, undefined if value was not found]
    */
-  _findOperationsFor (value) {
-    let operations = []
+  protected _findOperationsFor (value: V) {
+    let operations: OperationTuple3<V, T>[] = []
     if (this._values.has(value)) {
-      const isForValue = e => e.value === value
-      const notNull = e => e !== undefined
-      operations = [this._operations.find(isForValue)].filter(notNull)
+      const isForValue = (e: OperationTuple3<V, T>) => e.value === value
+      const notNull = (e?: OperationTuple3<V, T>) => e !== undefined
+
+      operations = [this._operations.find(isForValue)].filter(notNull) as OperationTuple3<V, T>[]
     }
     return operations
   }
@@ -243,34 +271,33 @@ export default class CmRDTSet extends Set {
    * @param  {[Object]} json [Input object to create the Set from. Needs to be: '{ values: [] }']
    * @return {[Set]}         [new Set instance]
    */
-  static from (json) {
-    return new CmRDTSet(json.values)
+  static from<V, T> (json: { values: { value: V, added: T[], removed: T[] }[] }): CmRDTSet<V, T> {
+    return new CmRDTSet<V, T>(json.values)
   }
 
   /**
    * Check if two Set are equal
    *
-   * Two Set are equal if they both contain exactly 
+   * Two Set are equal if they both contain exactly
    * the same values.
-   * 
+   *
    * @param  {[Set]}  a [Set to compare]
    * @param  {[Set]}  b [Set to compare]
    * @return {Boolean}  [True input Set are the same]
    */
-  static isEqual (a, b) {
-    return (a.toArray().length === b.toArray().length)
-      && a.hasAll(b.toArray())
+  static isEqual<V=unknown, T=unknown> (a: CmRDTSet<V, T>, b: CmRDTSet<V, T>) {
+    return (a.toArray().length === b.toArray().length) && a.hasAll(b.toArray())
   }
 
   /**
    * Return the difference between the values of two Sets
-   * 
+   *
    * @param  {[Set]} a [First Set]
    * @param  {[Set]} b [Second Set]
    * @return {[Set]}   [Set of values that are in Set A but not in Set B]
    */
-  static difference (a, b) {
-    const otherDoesntInclude = x => !b.has(x)
+  static difference<V=unknown, T=unknown> (a: CmRDTSet<V, T>, b: CmRDTSet<V, T>) {
+    const otherDoesntInclude = (x: V) => !b.has(x)
     const difference = new Set([...a.values()].filter(otherDoesntInclude))
     return difference
   }
--- src/utils.ts
+++ src/utils.ts
@@ -19,13 +19,13 @@ export class OperationTuple3<V=unknown, T=unknown> {
   readonly added: Set<T>
   readonly removed: Set<T>
 
-  constructor (value: V, added: Iterable<T>, removed: Iterable<T>) {
+  constructor (value: V, added?: Iterable<T>, removed?: Iterable<T>) {
     this.value = value
     this.added = new Set(added)
     this.removed = new Set(removed)
   }
 
-  static create<V=unknown, T=unknown> (value: V, added: Iterable<T>, removed: Iterable<T>): OperationTuple3<V, T> {
+  static create<V=unknown, T=unknown> (value: V, added?: Iterable<T>, removed?: Iterable<T>): OperationTuple3<V, T> {
     return new OperationTuple3(value, added, removed)
   }
 
